2b6476bf6efaa0df00b0f3f1d2a93a1853703e50
hecris <christopher.he33@myhunter.cuny.edu>
2018-10-01 21:59:41 -0400

    add wikipedia_wallpaper_changer.py

---
+++ dwc/wikipedia_wallpaper_changer.py
@@ -0,0 +1,30 @@
+import datetime
+import random
+
+import requests
+from bs4 import BeautifulSoup
+
+from utils import save_image, set_wallpaper_permanent
+
+date = datetime.date.today()
+url = 'https://commons.wikimedia.org/wiki/Main_Page'
+
+def getphotourl():
+    source = requests.get(url)
+    if source.status_code == 200:
+        soup = BeautifulSoup(source.text, 'html.parser')
+        potd_element = soup.find('div', {'id': 'mainpage-potd'})
+        photo_url = potd_element.find('img')['src']
+        return photo_url
+
+def picpath_wikipedia(saveDir, SHOW_DEBUG):
+    photo_url = getphotourl()
+    wikipedia_path = saveDir + 'Wikipedia:' + str(date) + str(random.randint(1, 100000)) + '.jpg'
+    if SHOW_DEBUG:
+        print('Download from:', photo_url)
+    savelink = save_image(photo_url, wikipedia_path, SHOW_DEBUG)
+    return savelink
+
+def change_wp(wp_wikipedia, saveDir, SHOW_DEBUG):
+    savelink = picpath_wikipedia(saveDir, SHOW_DEBUG)
+    set_wallpaper_permanent(savelink, SHOW_DEBUG)
